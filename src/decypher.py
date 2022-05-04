9be63e139d852d9e0bba43b654561907ea1e5767
rocco barbini <roccobarbi@gmail.com>
2022-05-04 23:39:02 +0200

    scoring and functioning (for aristocrats) greatly improved

--- src/decypher.py
+++ src/decypher.py
@@ -1,5 +1,6 @@
 import random
 import argparse
+import sys
 
 from fitness import calculate_fitness
 
@@ -7,6 +8,10 @@ from fitness import calculate_fitness
 RESTARTS = 5
 ITERATIONS = 10000
 ALPHABET = "abcdefghijklmnopqrstuvwxyz"
+MODES = {
+    "a": "aristocrat",
+    "p": "patristocrat"
+}
 
 
 def decode(text, key):
@@ -27,7 +32,7 @@ def extract_two(length):
     return first, second
 
 
-def iteration(ciphertext, language, alphabet, key_string):
+def iteration(ciphertext, language, alphabet, key_string, mode):
     first, second = extract_two(len(alphabet))
     temp_letter = key_string[first]
     key_list = []
@@ -39,12 +44,12 @@ def iteration(ciphertext, language, alphabet, key_string):
     for i in range(len(alphabet)):
         key_dict_temp[alphabet[i]] = key_list[i]
     plaintext = decode(ciphertext, key_dict_temp)
-    fitness = calculate_fitness(plaintext, language=language)
+    fitness = calculate_fitness(plaintext, language=language, mode=mode)
     key_string = ''.join([str(item) for item in key_list])
     return plaintext, fitness, key_string
 
 
-def restart_cycle(ciphertext, alphabet, language):
+def restart_cycle(ciphertext, alphabet, language, mode):
     key_list = list(alphabet)
     random.shuffle(key_list)
     key_string = ''.join([str(item) for item in key_list])
@@ -52,10 +57,10 @@ def restart_cycle(ciphertext, alphabet, language):
     for i in range(len(alphabet)):
         key_dict[ALPHABET[i]] = key_string[i]
     plaintext = decode(ciphertext, key_dict)
-    fitness = calculate_fitness(plaintext, language)
+    fitness = calculate_fitness(plaintext, language=language, mode=mode)
     for i in range(ITERATIONS):
-        text_temp, fitness_temp, key_string_temp = iteration(ciphertext, language, alphabet, key_string)
-        if fitness_temp > fitness:
+        text_temp, fitness_temp, key_string_temp = iteration(ciphertext, language, alphabet, key_string, mode)
+        if fitness_temp < fitness:
             plaintext = text_temp
             fitness = fitness_temp
             key_string = key_string_temp
@@ -73,16 +78,20 @@ def main():
                                  help="the number of iterations in each restart cycle")
     argument_parser.add_argument("--restarts", "-r", required=False, type=int, default=RESTARTS,
                                  help="the number of restart cycles")
+    argument_parser.add_argument("--mode", "-m", required=False, type=str, default="a",
+                                 choices=MODES.keys(), help="The type of cryptogram.")
     args = argument_parser.parse_args()
     ciphertext = args.text.lower()
     results = {}
     for tries in range(RESTARTS):
         print(str(tries))
-        results[tries] = restart_cycle(ciphertext, args.alphabet, args.language)
+        results[tries] = restart_cycle(ciphertext, args.alphabet, args.language, args.mode)
         print(str(tries) + ": " + str(results[tries][1]))
-    final_result = ("", 0)
+    final_result = None
     for result in range(len(results)):
-        if results[result][1] > final_result[1]:
+        if final_result is None:
+            final_result = results[result]
+        if results[result][1] < final_result[1]:
             final_result = results[result]
     print(final_result)
 
--- src/fitness.py
+++ src/fitness.py
@@ -6,31 +6,60 @@ from crypto_tools.stats.ngram_frequency import NgramFrequency
 DIFF_AMPLIFICATION = 15
 
 
-def calculate_fitness(text, language="en"):
+def fitness_aristocrat(text, language):
     lf = LetterFrequency(text).calculate()
     bf = NgramFrequency(text=text, nvalue=2).calculate()
-    fitness = 100
+    fitness = 0
     for letter in lf:
         if str(letter).isalpha():
-            fitness -= abs(data.letter_frequency_by_language[language][letter] - lf[letter]) * DIFF_AMPLIFICATION
+            fitness += abs(data.letter_frequency_by_language[language][letter] - lf[letter]) * DIFF_AMPLIFICATION
     for bigram in bf:
         if bigram not in data.bigram_frequency_by_language[language].keys():
-            fitness -= bf[bigram] * DIFF_AMPLIFICATION
+            fitness += bf[bigram] * DIFF_AMPLIFICATION
         else:
-            fitness -= abs(data.bigram_frequency_by_language[language][bigram] - bf[bigram]) * DIFF_AMPLIFICATION
+            fitness += abs(data.bigram_frequency_by_language[language][bigram] - bf[bigram]) * DIFF_AMPLIFICATION
+    words = text.split()
+    for word in words:
+        if len(word) < 6 and word in data.frequent_short_words_by_language[language][len(word)]:
+            fitness -= 0.5
+    return fitness
+
+
+def fitness_patristocrat(text, language):
+    lf = LetterFrequency(text).calculate()
+    bf = NgramFrequency(text=text, nvalue=2).calculate()
+    fitness = 0
+    for letter in lf:
+        if str(letter).isalpha():
+            fitness += abs(data.letter_frequency_by_language[language][letter] - lf[letter]) * DIFF_AMPLIFICATION
+    for bigram in bf:
+        if bigram not in data.bigram_frequency_by_language[language].keys():
+            fitness += bf[bigram] * DIFF_AMPLIFICATION
+        else:
+            fitness += abs(data.bigram_frequency_by_language[language][bigram] - bf[bigram]) * DIFF_AMPLIFICATION
     for index in range(len(text) - 1):
         if text[index:index] in data.frequent_short_words_by_language[language][1]:
-            fitness += 0.5
+            fitness -= 0.5
     for index in range(len(text) - 2):
-        if text[index:index+1] in data.frequent_short_words_by_language[language][2]:
-            fitness += 0.5
+        if text[index:index + 1] in data.frequent_short_words_by_language[language][2]:
+            fitness -= 0.5
     for index in range(len(text) - 3):
-        if text[index:index+2] in data.frequent_short_words_by_language[language][3]:
-            fitness += 0.5
+        if text[index:index + 2] in data.frequent_short_words_by_language[language][3]:
+            fitness -= 0.5
     for index in range(len(text) - 4):
-        if text[index:index+3] in data.frequent_short_words_by_language[language][4]:
-            fitness += 0.5
+        if text[index:index + 3] in data.frequent_short_words_by_language[language][4]:
+            fitness -= 0.5
     for index in range(len(text) - 5):
-        if text[index:index+4] in data.frequent_short_words_by_language[language][5]:
-            fitness += 0.5
+        if text[index:index + 4] in data.frequent_short_words_by_language[language][5]:
+            fitness -= 0.5
     return fitness
+
+
+FITNESS_MODES = {
+    "a": fitness_aristocrat,
+    "p": fitness_patristocrat
+}
+
+
+def calculate_fitness(text, language="en", mode="a"):
+    return FITNESS_MODES[mode](text, language)
